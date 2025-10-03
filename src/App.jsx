9fb454a8146d50b3d3d9bbbbe8711d8519c7249d
Ulul Azmi <ululazmiua02@gmail.com>
2025-10-04 00:41:45 +0700

    Initial File and configurasi TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-warna-ulul text-blue-700">
+        Hello world!
+      </h1>
+    </>
+  );
+}
