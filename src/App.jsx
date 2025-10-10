8ee41539c1782660960b4c47057fa43241a754d5
Ulul Azmi <ululazmiua02@gmail.com>
2025-10-10 16:55:11 +0700

    Add react-router,react-use,and set background

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-warna-ulul text-blue-700">
-        Hello world!
-      </h1>
+     
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Layout from "./Layout.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />} >
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
